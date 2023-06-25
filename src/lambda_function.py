0a66cd09e9e8d9ccebeb3f193c13b2f4a932f404
Ashutosh <ashutosh.baruah2@escortskubota.com>
2023-06-25 14:43:44 +0530

    Commit of building the docker image using codepipeline

---
+++ src/lambda_function.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+def lambda_handler(event context):
+
+    {
+        return {
+            "statuscode" : 200,
+            "body" : json.dumps("Success")
+        }
+    }
