cf6de0132847ee94884b00d526e2c701e6335904
Ashutosh Baruah <ashutosh.baruah2@escortskubota.com>
2023-09-17 20:17:37 +0000

    Edited lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,4 +1,4 @@
-
+'''
 import json
 import os
 import boto3
@@ -159,7 +159,7 @@ from PIL import Image
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     bucket_name = 'gis-colourized-png-data'
-    object_key = 'colour.png'
+    object_key = 'colorized_278_finaltesting..png'
     
     try:
         s3_response = s3.get_object(Bucket=bucket_name, Key=object_key)
@@ -182,4 +182,3 @@ def lambda_handler(event, context):
         'body': base64.b64encode(image_data).decode('utf-8'),
         'isBase64Encoded': True
     }
-'''
